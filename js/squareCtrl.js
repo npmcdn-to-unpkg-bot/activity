698a2888ec7356912c92db62c9ff53d4c62f075a
v_gaoxchen <v_gaoxchen@tencent.com>
2016-07-24 19:49:14 +0800

    13302010055

--- js/squareCtrl.js
+++ js/squareCtrl.js
@@ -1,6 +1,13 @@
 angular.module('squareCtrl',[])
 	.controller('squareCtrl',function(){
-		
+		// alert('ffff');
+		$('.tttt').pullToRefresh();
+		$('.tttt').on("pull-to-refresh", function() {
+			setTimeout(function(){
+				$('.tttt').pullToRefreshDone();
+			},5000);
+			
+		});
 
 
 	})
